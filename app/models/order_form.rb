d86bdcf082534c243bdeb73e6b46fc7797fe8f50
Higuchi-Hiroyoshi <h2.hero.hh@gmail.com>
2021-03-15 16:08:43 +0900

    API導入／クレジットカード決済での購入機能を実装する／モデルの単体テストコードを書く

---
+++ app/models/order_form.rb
@@ -0,0 +1,22 @@
+class OrderForm < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :zip_code, :shipping_area_id, :city, :street_address, :building, :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  validates :shipping_area_id, numericality: { other_than: 1 }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    address = Address.create(zip_code: zip_code, shipping_area_id: shipping_area_id, city: city, street_address: street_address,
+                             building: building, phone_number: phone_number, order: order)
+  end
+end
