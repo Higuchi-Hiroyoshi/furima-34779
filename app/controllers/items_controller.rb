9c37008432bfaa9d4d123bb1ab29ca68e7051186
Higuchi-Hiroyoshi <h2.hero.hh@gmail.com>
2021-03-16 14:50:24 +0900

    各機能再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:update]
+  before_action :move_to_index, only: [:update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
